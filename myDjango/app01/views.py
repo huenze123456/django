f75201f265f6de9b33c36e689400871f26984f2a
huenze <huenze@huenze.localdomain>
2021-03-02 20:00:15 +0800

    change views

--- myDjango/app01/views.py
+++ myDjango/app01/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("第一个页面")
+    return HttpResponse("这是wode第一个页面")
 def index2(request):
-    return HttpResponse("第二个页面")
+    return HttpResponse("这是wode第二个页面")
